46fc43f9aa5d298501befe47adfa60834c876ba7
nackalalalong <nak-online@hotmail.com>
2026-04-13 20:46:42 +0700

    fix: fix mock loadtest service name not reachable

---
+++ system/loadtest_mock_provider/start.sh
@@ -0,0 +1,4 @@
+docker compose -f system/loadtest_mock_provider/docker-compose.yaml up -d --build
+cd voicebot
+cargo build --release -p voicebot-loadtest
+cargo run --release -p voicebot-loadtest -- ../system/loadtest_mock_provider/loadtest.toml
