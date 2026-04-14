e7428feba43ce325ae94bc60e044994c9e7b86d2
nackalalalong <nak-online@hotmail.com>
2026-04-14 19:58:42 +0700

    chore: addjust loadtest params

--- system/loadtest_mock_provider/start.sh
+++ system/loadtest_mock_provider/start.sh
@@ -1,3 +1,4 @@
+mkdir -p system/loadtest_mock_provider/artifacts/loadtest
 docker compose -f system/loadtest_mock_provider/docker-compose.yaml up -d --build
 cd voicebot
 cargo build --release -p voicebot-loadtest
